0f7c6fc5ab2fd8349b083325659f6d588c3cad40
gKonstantin <gkonstantin17@ya.ru>
2025-10-04 22:40:52 +0300

    task-2: entities

--- task-2/file-server/src/main/java/org/dock/entity/FileMetadata.java
+++ task-2/file-server/src/main/java/org/dock/entity/FileMetadata.java
@@ -1,4 +1,21 @@
 package org.dock.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileMetadata {
+    private Long id;
+    private String uniqueId;
+    private String originalName;
+    private String storedName;
+    private Long size;
+    private String contentType;
+    private Long userId;
+    private Long uploadedAt;
+    private Long lastDownloadedAt;
+    private Integer downloadCount;
 }
--- task-2/file-server/src/main/java/org/dock/entity/User.java
+++ task-2/file-server/src/main/java/org/dock/entity/User.java
@@ -1,4 +1,16 @@
 package org.dock.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
+    private String username;
+    private String passwordHash;
+    private String token;
+    private Long createdAt;
 }
