012e371424ed16d86ab4503bc39fb85533619a71
gKonstantin <gkonstantin17@ya.ru>
2025-10-04 20:24:41 +0300

    create entities

--- task-1/bottles/src/main/java/org/dock/entity/Move.java
+++ task-1/bottles/src/main/java/org/dock/entity/Move.java
@@ -1,4 +1,16 @@
 package org.dock.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Move {
+    private int fromBottle;
+    private int toBottle;
+
+    @Override
+    public String toString() {
+        return String.format("(%2d, %2d)", fromBottle, toBottle);
+    }
 }
