b391bfa3645b9fb63c22206bc3923fb0c9fb7f59
gKonstantin <gkonstantin17@ya.ru>
2025-10-05 03:38:19 +0300

    task-3: starts

--- task-3/weather-app/src/main/java/org/dock/Main.java
+++ task-3/weather-app/src/main/java/org/dock/Main.java
@@ -1,7 +1,34 @@
 package org.dock;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.dock.controller.WeatherController;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/weather", new WeatherController());
+
+        server.createContext("/static", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String path = exchange.getRequestURI().getPath();
+                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+                byte[] response = "Static file serving".getBytes();
+                exchange.sendResponseHeaders(200, response.length);
+                exchange.getResponseBody().write(response);
+                exchange.getResponseBody().close();
+            }
+        });
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port " + port);
     }
 }
--- task-3/weather-app/src/main/java/org/dock/сonfig/AppConfig.java
+++ task-3/weather-app/src/main/java/org/dock/сonfig/AppConfig.java
@@ -1,4 +1,17 @@
 package org.dock.сonfig;
 
+import lombok.Data;
+
+@Data
 public class AppConfig {
+    private static final int SERVER_PORT = 8080;
+    private static final int CACHE_TTL_MINUTES = 15;
+
+    public static int getServerPort() {
+        return SERVER_PORT;
+    }
+
+    public static int getCacheTtlMinutes() {
+        return CACHE_TTL_MINUTES;
+    }
 }
